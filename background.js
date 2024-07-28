a0541f664464132899d54c9d784f8abe6223a4d8
Chitrarth <chitrarthrai10@gmail.com>
2024-07-28 18:55:54 +0530

    changes for youtube

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Hover Video Control Extension Installed");
-  });
-  
+  console.log("Hover Video Control Extension Installed");
+});
--- content.js
+++ content.js
@@ -1,30 +1,48 @@
 function controlVideoPlayback(event) {
-    if (event.type === 'mouseover') {
-      event.target.play();
-    } else if (event.type === 'mouseout') {
-      event.target.pause();
+  if (event.type === 'mouseover') {
+    event.target.play();
+  } else if (event.type === 'mouseout') {
+    event.target.pause();
+  }
+}
+
+function addHoverControl(video) {
+  video.addEventListener('mouseover', controlVideoPlayback);
+  video.addEventListener('mouseout', controlVideoPlayback);
+}
+
+function removeHoverControl(video) {
+  video.removeEventListener('mouseover', controlVideoPlayback);
+  video.removeEventListener('mouseout', controlVideoPlayback);
+}
+
+function toggleHoverControl(enable) {
+  const videos = document.querySelectorAll('video, #movie_player video');
+  videos.forEach(video => {
+    if (enable) {
+      addHoverControl(video);
+    } else {
+      removeHoverControl(video);
     }
-  }
-  
-  function addHoverControl(video) {
-    video.addEventListener('mouseover', controlVideoPlayback);
-    video.addEventListener('mouseout', controlVideoPlayback);
-  }
-  
-  // Select all video elements on the page and add hover control
-  const videos = document.querySelectorAll('video');
-  videos.forEach(video => addHoverControl(video));
-  
+  });
+}
+
+chrome.storage.sync.get('hoverControlEnabled', (data) => {
+  const isEnabled = data.hoverControlEnabled ?? true; // Default to true if not set
+  toggleHoverControl(isEnabled);
+
   // Listen for dynamically added videos
   const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       mutation.addedNodes.forEach(node => {
-        if (node.nodeName === 'VIDEO') {
-          addHoverControl(node);
+        if (node.nodeName === 'VIDEO' || node.matches && node.matches('#movie_player video')) {
+          if (isEnabled) {
+            addHoverControl(node);
+          }
         }
       });
     });
   });
-  
+
   observer.observe(document.body, { childList: true, subtree: true });
-  
+});
